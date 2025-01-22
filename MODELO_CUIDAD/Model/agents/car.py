8286fb2497241b7abd0340ab851d413d6dcbf506
Miguel Cabrera Victoria <miguelangelcabreravictoria@gmail.com>
2025-01-22 14:47:50 -0600

    feat(agents): Add UML structure into Car(agent)

--- MODELO_CUIDAD/Model/agents/car.py
+++ MODELO_CUIDAD/Model/agents/car.py
@@ -3,7 +3,75 @@ import agentpy as ap
 
 class Car(ap.Agent):
     def setup(self):
+        self.position = None
+        self.speed = self.p.cars_initial_speed
+        self.destinity
+        self.status = ""
+        self.fuel = self.p.cars_initial_fuel
+        self.path = []
+        self.mailbox = []
+
+    def get_postion(self):
+        """
+        Obtiene la posicion actual del coche 
+        """
+        pass
+
+    def avoid_collision(self):
+        """
+        Evita colisiones detectando agentes cercanos
+        """
+        pass
+
+    def calculate_path(self):
+        """
+        Calcula el camino optimo hacia el destino del coche
+        """
+        pass
+
+    def wait_semaphore(self):
+        """
+        Espera en el semaforo si esta en rojo
+        """
+        pass
+
+    def search_gas_station(self):
+        """
+        Busca la estacion de servicio mas cercana si el combustible es bajo
+        """
+        pass
+
+    def refill_fuel(self):
+        """
+        Recarga combustible en un estacion de servicio
+        """
+        pass
+
+    def in_destiny(): 
+        """
+        Verifica si el coche ha llegado a su destino
+        """
+        pass 
+
+    def move(self):
+        """
+        Mueve el coche en funcion de su velocidad y camino calculado
+        """
+        pass
+
+    def communicate(self, recipient, message):
+        """
+        @param recipient: Agente destinatario del mensaje
+        @param message: Contenido del mensaje
+
+        Envia un mensaje a otro agente
+
+        """
         pass
 
     def execute(self):
+        """
+        Ejecuta las acciones del coche en cada paso de simulacion
+
+        """
         pass
