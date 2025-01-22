5847af548bfd117baa8dbca5ca1e99913e02b9d3
Miguel Cabrera Victoria <miguelangelcabreravictoria@gmail.com>
2025-01-22 14:48:21 -0600

    feat(agents): Add UML structure into Person(Agent)

--- MODELO_CUIDAD/Model/agents/person.py
+++ MODELO_CUIDAD/Model/agents/person.py
@@ -2,7 +2,30 @@ import agentpy as ap
 
 class Person(ap.Agent):
     def setup(self):
+        self.position = None
+        self.destinity = None
+        self.status 
+        self.path = []
+        self.mailbox = []
+
+    def get_position(self):
+        """
+        Obtiene la posicion actual del individuo
+        """
         pass
 
+    def calculate_path(self):
+        """
+        Calcula el camino optimo hacia el destino del individuo
+        """
+
+    def in_destiny(self):
+        """
+        Verifica si el individuo ha llegado a su destino
+        """
+
     def execute(self):
+        """
+        Ejecuta las acciones del individuo en cada paso de la simulacion
+        """
         pass
