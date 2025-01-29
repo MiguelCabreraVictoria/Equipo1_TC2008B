201bffcd6b981708d0cb76b18e6773de087912bf
Miguel Cabrera Victoria <miguelangelcabreravictoria@gmail.com>
2025-01-29 17:45:46 -0600

    fix(dtos): Agents status corrected

--- MODELO_CUIDAD/Model/model_types/dtos/CarStatus.py
+++ MODELO_CUIDAD/Model/model_types/dtos/CarStatus.py
@@ -2,7 +2,9 @@ from enum import Enum
 
 
 class CarStatus(Enum):
+
     IN_MOVEMENT = "moving"
     STOPPED = "stopped"
     PARKED = "parked"
     WAITING = "waiting"
+    IN_DESTINY = "in_destiny"
