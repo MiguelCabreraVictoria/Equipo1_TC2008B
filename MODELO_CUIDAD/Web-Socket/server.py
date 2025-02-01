e9a3f8699c073a3c249869e5af9e63de1c4eb3a7
Miguel Cabrera Victoria <miguelangelcabreravictoria@gmail.com>
2025-02-01 12:17:06 -0600

    fix(web_socket): start code method

--- MODELO_CUIDAD/Web-Socket/server.py
+++ MODELO_CUIDAD/Web-Socket/server.py
@@ -1,7 +1,6 @@
 import asyncio
 import websockets
 
-
 connected_clients = set()
 
 async def ws_server(websocket):
@@ -9,20 +8,21 @@ async def ws_server(websocket):
     try:
         async for message in websocket:
             for client in connected_clients:
-                # Enviar el mensaje a todos los clientes conectados excepto a quien lo envió
-                if client != websocket:
+                # print(f"Received: {message}")
+                if client != websocket:  # No enviar el mensaje al remitente
                     await client.send(message)
+                    print(f'{message} enviado a {client}')
     except websockets.exceptions.ConnectionClosed:
         print("Connection closed")
     finally:
         connected_clients.remove(websocket)
 
-async def send_message():
+async def main():
     # Generar handshake
     server = await websockets.serve(ws_server, "localhost", 8765)
     print("Servidor WebSocket corriendo en ws://localhost:8765")
     # Esperar a que el servidor se cierre
     await server.wait_closed()
 
-
-asyncio.run(send_message())
+if __name__ == "__main__":
+    asyncio.run(main())
