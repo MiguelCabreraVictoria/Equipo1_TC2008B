a8c243f623c01cc9a6d71f5874d91a20afdc2a12
Miguel Cabrera Victoria <miguelangelcabreravictoria@gmail.com>
2025-02-04 10:01:26 -0600

    feat(Unity)

--- MODELO_CUIDAD/City/Assets/UnityClient.cs
+++ MODELO_CUIDAD/City/Assets/UnityClient.cs
@@ -1,16 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using WebSocketSharp;
+using Newtonsoft.Json.Linq;
 using UnityEngine;
 
 public class UnityClient : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public string url = "ws://localhost:8765";
+    private WebSocket ws;
+
     void Start()
     {
+        ws = new WebSocket(url);
         
+    
+        ws.OnMessage += (sender, e) =>
+        {
+            try
+            {
+                JObject data = JObject.Parse(e.Data);
+
+                
+                int id = (int)data["id"];
+                string type = (string)data["type"];  
+                string status = (string)data["status"];
+                int x = (int)data["x"];
+                int y = (int)data["y"];
+                int z = (int)data["z"];
+
+                Debug.Log($"Mensaje recibido: ID={id}, Tipo={type}, Estado={status}, Posición=({x}, {y}, {z})");
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError("Error al procesar el mensaje WebSocket: " + ex.Message);
+            }
+        };
+
+    
+        ws.Connect();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnApplicationQuit()
     {
-        
+        if (ws != null)
+        {
+            ws.Close();
+            Debug.Log("Conexión WebSocket cerrada.");
+        }
     }
 }
